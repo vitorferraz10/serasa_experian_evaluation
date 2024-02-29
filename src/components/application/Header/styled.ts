ee3e17c4fa2255c5f0dd5276ae75c817ade3af2c
Vitor Ferraz <vitor.maciel@fretebras.com.br>
2024-02-29 12:42:24 -0300

    fix: size header

--- src/components/application/Header/styled.ts
+++ src/components/application/Header/styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ContainerHeader = styled.div`
   width: 100vw;
-  height: 100px;
+  height: 64px;
   background-color: #000;
   display: flex;
   justify-content: center;
