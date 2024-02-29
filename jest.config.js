a1b29fe8b6d2e1d80ebd5c8652d7f17d565dc031
Vitor Ferraz <vitor.maciel@fretebras.com.br>
2024-02-29 12:34:32 -0300

    test: add ci

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,8 @@ const customJestConfig = {
   moduleNameMapper: {
     '^test/(.*)$': '<rootDir>/src/test/$1', 
     '^styles/(.*)$': '<rootDir>/src/styles/$1', 
+    '^store/(.*)$': '<rootDir>/src/store/$1',
+    '^service/(.*)$': '<rootDir>/src/service/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/src/test/jest.setup.ts'],
   coveragePathIgnorePatterns: [
